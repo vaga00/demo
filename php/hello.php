5c523a80359abc142d1d1edc27e028fc97a2e876
vaga.Tsai <vaga0955@gmail.com>
2015-03-23 17:31:37 +0800

    First commit to start this plan.

---
+++ php/hello.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Author: vaga Tsai
+* Start: 2015-03-23
+* Brief: just to leave a library of some sample function
+* 
+**/
+
+$welcome_msg = "Welcome to my Git. All of source code included in this folder are free.";
+
+echo $welcome_msg;
+?>
